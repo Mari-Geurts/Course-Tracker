ea88268aa721b450f356b6247e0806cd63ebea66
Mari-Geurts <mgeurts72001@gmail.com>
2024-02-16 09:57:31 -0500

    Update to read date and calculate avg assignments to complete a day

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 A scuffed course tracker for BootDev
 A project I am making to keep track of the assignments I completed and still have yet to complete in boot.Dev
 
-Paste into console on boot.dev courses page:
-```Java
+Update 2/16/24: Program now reads your current date, and calculates how many assignments should be completed in a day to finish before license expires.
+
+Paste into console on boot.dev courses page to get assignments info:
+
+```JavaScript
 let count = document.querySelector("#__nuxt > div > div.static-bgimage > div > div.flex.h-full.flex-1.flex-col.overflow-auto.align-top > div > div > div.max-w-5xl.flex-1 > section > div > div:nth-child(3) > div:nth-child(4)").querySelectorAll("span.mb-1");
 let mySum = 0;
 let total = 0;
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
+from datetime import datetime
+from datetime import date
+import math
+
 #total course count as of 02/14/24
 total_courses = 1226
 
-#Completed problems as of 02/14/24
-problems_completed = 308
+#Completed problems as of 02/15/24
+problems_completed = 318
 
-#Function to calculate incomplete assignments owo
+#Function to calculate incomplete assignments
 def get_incomplete_courses(completed):
-    incomplete = total_courses - completed
-    return incomplete
+    return total_courses - completed
 
-print(f"Total Assignments: {total_courses}")
-print(f"Assignments Completed: {problems_completed}")
+
+joined_date = {
+      "Month": 12,
+      "Day": 1,
+      "Year": 2023,
+      "FullDate": date(2023,12,1),
+}
+
+end_date = {
+      "Month": 12,
+      "Day": 1,
+      "Year": 2024,
+      "FullDate": date(2024,12,1),
+}
+
+
+#print(f"Total Assignments: {total_courses}")
+#print(f"Assignments Completed: {problems_completed}")
 print(f"Remaining Assignments: {get_incomplete_courses(problems_completed)}")
+
+#Stores today date in instanced object
+today_date = datetime.today()
+
+year = today_date.year
+month = today_date.month
+day = today_date.day
+todays_date = date(year, month, day)
+
+
+def get_days_left():
+    difference_in_days = (end_date["FullDate"] - todays_date).days
+    #print(f"Difference in Days: {difference_in_days}")
+    return difference_in_days
+
+def get_avg_assignments(difference):
+    avg = (get_incomplete_courses(problems_completed)) / difference
+    print(f"Average Assignments to complete a day: {math.ceil(avg)}")
+    return avg
+
+get_avg_assignments(get_days_left())
